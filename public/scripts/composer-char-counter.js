25a7c25f7cd0defd8ab6c890d90110b01aea6b53
bhavBains <deepbains26@gmail.com>
2017-11-15 01:09:01 +0000

    Added character counter functionality, and added new article for recent tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('#tweet-textarea').on('keyup', characterCount);
+  $('#tweet-textarea').on('paste', characterCount);   
+})  
+
+//To count Character of the tweet
+function characterCount(){
+  var counter = $(this).parents(".container").find(".counter")
+  var length = $(this).val().length;
+  var maxValue = 140;
+  if (length >= 140) {
+    counter.text((maxValue-length)).css({color:'red'});
+  } else {
+    counter.text((maxValue-length)).css({color:'black'});  
+  }
+}
